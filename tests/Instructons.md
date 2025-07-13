99bf8f9a7677de6b8b84bca487b260c654f3748c
Christopher Peacock <peacockchristopher15@mail.com>
2025-07-13 16:48:54 +0100

    feat: refactored tests and made an instructiosn mark down file

---
+++ tests/Instructons.md
@@ -0,0 +1,7 @@
+## Command to start the test proceedure
+
+# start test proceedure
+```bash
+.\.venv\Scripts\python.exe -m pytest
+```
+
--- tests/test_embed.py
+++ tests/test_embed.py
@@ -1,11 +1,11 @@
-from utils.embed import run_nmap
-from utils.embed import run_ping
+from tools import nmap, ping
+
 
 def test_nmap_accepts_target():
-    result = run_nmap("127.0.0.1") 
+    result = nmap.run_nmap("127.0.0.1") 
     assert "nmap scan results" in result.lower() 
 
 
 def test_can_ping_target():
-    result = run_ping("google.com")
+    result = ping.run_ping("google.com")
     assert "is reachable" in result.lower()
