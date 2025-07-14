11d8209bf2bad54c41ec73b240a3d96237add0d2
Christopher Peacock <peacockchristopher15@mail.com>
2025-07-14 22:01:22 +0100

    feat: resturctre files and put in debug flat on args.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 import argparse
 import os
+import json
+from pathlib import Path
 from utils.scrape import scrape_url
 from utils.embed import get_vectorstore, query_jarvis
 from dotenv import load_dotenv
 from visuals import eyes
 
 load_dotenv()
+
+def load_config(path='config.json'):
+    if Path(path).exists():
+        print("found file")
+        with open(path) as f:
+            return json.load(f)
+
 
 def interactive_loop():
     
@@ -25,12 +34,15 @@ def interactive_loop():
             break
 
 def main():
+    config = load_config()
     eyes.animate_eyes()
     print(f"\n[E.V.A] Hello {os.getenv('NAME')}. What are we doing today?")
-    
+
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--ingest', help='URL to scrape and vectorise')
     parser.add_argument('--ask', help='Ask a question to your assistant')
+    parser.add_argument('--debug', help='Do you want to put the debug mode on' )
     args = parser.parse_args()
     
     if args.ingest:
@@ -45,6 +57,10 @@ def main():
         answer = query_jarvis(args.ask, stream=True)
         print("\n")
     
+    if args.debug:
+        debug = args.debug if args.debug else config.get("debug", False)
+        print(f"debug mode is on:")
+        
     interactive_loop()
 
 if __name__ == '__main__':
